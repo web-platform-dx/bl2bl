de9e7811c77af520b07916c0f4fd3ec0daed3daf
Tony Conway <tony.p.conway@gmail.com>
2024-12-05 15:26:13 +0000

    code clean up with Prettier and add License

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ To update your `browserslist` config, run the following command:
 npx bl2bl
 ```
 
-`bl2bl` uses `@mdn/browser-compat-data` as a data source and will install the latest version of `@mdn/browser-compat-data` on install.  However, is strongly recommended that you update `@mdn/browser-compat-data` regularly, or every time you run `npx bl2bl`, especially if you are targeting Baseline Widely available.  You can automate this by adding a command to your `package.json` `scripts` array which updates `@mdn/browser-compat-data` before running `npx bl2bl`:
+`bl2bl` uses `@mdn/browser-compat-data` as a data source and will install the latest version of `@mdn/browser-compat-data` on install. However, is strongly recommended that you update `@mdn/browser-compat-data` regularly, or every time you run `npx bl2bl`, especially if you are targeting Baseline Widely available. You can automate this by adding a command to your `package.json` `scripts` array which updates `@mdn/browser-compat-data` before running `npx bl2bl`:
 
 ```
 "scripts": {
@@ -45,22 +45,22 @@ If you don't add a `bl2bl` property to your `package.json` file, the following d
 
 This property determines which browsers your target and can take two value types: a string reading `widely available` or an integer representing the Baseline year required in the format `YYYY`.
 
-Using `widely available` will select the last version of each of the Baseline core browser set released *exactly 30 months before the time of execution*.
+Using `widely available` will select the last version of each of the Baseline core browser set released _exactly 30 months before the time of execution_.
 
-Using a year in the format `YYYY` will select the last version of each of the Baseline core browser set released in the specified year.  This may change in future releases to select earlier version of browsers which support the full Baseline feature set that calendar year.
+Using a year in the format `YYYY` will select the last version of each of the Baseline core browser set released in the specified year. This may change in future releases to select earlier version of browsers which support the full Baseline feature set that calendar year.
 
 If you pass an invalid string, or an integer below 2016 or above the previous calendar year, `bl2bl` will throw an error and not process any changes.
 
 ### `useBrowserslistrc`
 
-`bl2bl` defaults to storing `browserslist` configurations in the projects `package.json` file.  If you want to store your `browserslist` configuration using the `.browserslistrc` file format, set this property to `true`.
+`bl2bl` defaults to storing `browserslist` configurations in the projects `package.json` file. If you want to store your `browserslist` configuration using the `.browserslistrc` file format, set this property to `true`.
 
 ### `downstreamBrowsers`
 
-There are many browsers that depend on another browser's engine and therefore support an identical feature set.  For example, [Opera 115 implements Blink 129](https://github.com/mdn/browser-compat-data/blob/037fce457e530715679ce4ae4b318aa18904bea8/browsers/opera.json#L863) and therefore has the same web platform feature support as Chrome 129.  Setting `downstreamBrowsers` to true will select minimum browser versions from other vendors that correspond to the minimum versions of the core browser set browsers you have selected.
+There are many browsers that depend on another browser's engine and therefore support an identical feature set. For example, [Opera 115 implements Blink 129](https://github.com/mdn/browser-compat-data/blob/037fce457e530715679ce4ae4b318aa18904bea8/browsers/opera.json#L863) and therefore has the same web platform feature support as Chrome 129. Setting `downstreamBrowsers` to true will select minimum browser versions from other vendors that correspond to the minimum versions of the core browser set browsers you have selected.
 
 TODO: include an example here
 
 ### `savePrevious`
 
-By default, `bl2bl` will save your previous `browserlist` configuration in a file called `.browserslistrc_backup` when you run `npx bl2bl`, in case you need to roll back to a previous configuration.  If you don't want to create this backup, set `savePrevious` to false.
+By default, `bl2bl` will save your previous `browserlist` configuration in a file called `.browserslistrc_backup` when you run `npx bl2bl`, in case you need to roll back to a previous configuration. If you don't want to create this backup, set `savePrevious` to false.
--- bin/cli.js
+++ bin/cli.js
@@ -1,122 +1,140 @@
 #!/usr/bin/env node
-import('baseline-browser-mapping').then(bbm => {
+import("baseline-browser-mapping").then((bbm) => {
+  // This list represents browsers that are supported in baseline-browser-mapping AND browserslist
+  const browsers = {
+    chrome: "Chrome",
+    chrome_android: "ChromeAndroid",
+    edge: "Edge",
+    firefox: "Firefox",
+    firefox_android: "FirefoxAndroid",
+    safari: "Safari",
+    safari_ios: "iOS",
+    webview_android: "Android",
+    samsunginternet_android: "Samsung",
+    opera_android: "op_mob",
+    opera: "Opera",
+    qq_android: "and_qq",
+    uc_android: "and_uc",
+  };
 
-    // This list represents browsers that are supported in baseline-browser-mapping AND browserslist
-    const browsers = {
-        "chrome": "Chrome",
-        "chrome_android": "ChromeAndroid",
-        "edge": "Edge",
-        "firefox": "Firefox",
-        "firefox_android": "FirefoxAndroid",
-        "safari": "Safari",
-        "safari_ios": "iOS",
-        "webview_android": "Android",
-        "samsunginternet_android": "Samsung",
-        "opera_android": "op_mob",
-        "opera": "Opera",
-        "qq_android": "and_qq",
-        "uc_android": "and_uc",
-    };
+  const packageJSON = require(process.cwd() + "/package.json");
 
-    const packageJSON = require(process.cwd() + '/package.json');
+  const fs = require("node:fs");
 
-    const fs = require('node:fs');
+  if (packageJSON.bl2bl.savePrevious) {
+    let backupMessage =
+      "# backup file created by bl2bl at " + new Date().toLocaleString() + "\n";
 
-    if (packageJSON.bl2bl.savePrevious) {
-        let backupMessage = "# backup file created by bl2bl at " + new Date().toLocaleString() + '\n';
-
-        // Check if .browserslistrc exists
-        if (fs.existsSync(process.cwd() + '/.browserslistrc')) {
-            // Read the contents of .browserslistrc
-            let browserslistrcData = fs.readFileSync(process.cwd() + '/.browserslistrc', { encoding: 'utf8' });
-            // And write it to a backupfile
-            fs.writeFileSync(process.cwd() + '/.browserslistrc_backup', (backupMessage + browserslistrcData), { encoding: 'utf8' });
-        } else {
-            // Check if packageJSON.browserslist exists
-            if (packageJSON.browserslist) {
-                // Write each item of the array on a new line in .browserslistrc_backup
-                const backupData = packageJSON.browserslist.join('\n');
-                fs.writeFileSync(process.cwd() + '/.browserslistrc_backup', (backupMessage + backupData), { encoding: 'utf8' });
-            } else {
-                console.log("No existing browserslist configuration, skipping backup.");
-            }
-        }
-
-    }
-
-    let baselineVersions;
-    let browserslistOutput = new Array();
-
-    const bl2blConfig = packageJSON.bl2bl;
-
-    // If the user wants Baseline Widely Available
-    if (
-        bl2blConfig.baselineThreshold === "widely available"
-    ) {
-        // Get the minimum Baseline widely available versions
-        baselineVersions = bbm.getMinimumWidelyAvailable(packageJSON.bl2bl.downstreamBrowsers);
-    } else if ((
-        // If they've passed a valid year, i.e. after BL starts and not the current or future years
-        parseInt(bl2blConfig.baselineThreshold) >= 2016
-        &&
-        parseInt(bl2blConfig.baselineThreshold) <= (new Date().getFullYear() - 1)
-    )) {
-        // Get the versions that correspond to those years
-        baselineVersions = bbm.getMinimumByYear(bl2blConfig.baselineThreshold, bl2blConfig.downstreamBrowsers);
-    }
-    else {
-        // If the baselineThreshold doesn't meet any of those criteria, there's a problem.  Time to bounce.
-        console.warn("there's a problem with your bl2bl config.\nPlease fix it before proceeding.")
-        process.exit();
-    }
-
-    // Loop through returned minimum browser versions
-    baselineVersions.forEach(browser => {
-        // Check if that browser is supported by Browserslist
-        if (Object.keys(browsers).includes(browser.browser)) {
-            // And add it to the list of versions to be written
-            browserslistOutput.push(`${browsers[browser.browser]} >= ${browser.version}`)
-        }
-    });
-
-
-    // Behaviour varies depending on whether userBrowserslistrc=true or false
-    if (!bl2blConfig.useBrowserslistrc) {
-        // if false, add baselineVersions to packageJSON object for later
-        packageJSON['browserslist'] = browserslistOutput;
-        // and delete any existing .browserslistrc files to avoid conflict
-        fs.access(process.cwd() + '/.browserslistrc', err => {
-            if (!err) {
-                fs.unlink(process.cwd() + '/.browserslistrc', (err) => {
-                    if (err) throw err;
-                });
-            }
-        })
+    // Check if .browserslistrc exists
+    if (fs.existsSync(process.cwd() + "/.browserslistrc")) {
+      // Read the contents of .browserslistrc
+      let browserslistrcData = fs.readFileSync(
+        process.cwd() + "/.browserslistrc",
+        { encoding: "utf8" },
+      );
+      // And write it to a backupfile
+      fs.writeFileSync(
+        process.cwd() + "/.browserslistrc_backup",
+        backupMessage + browserslistrcData,
+        { encoding: "utf8" },
+      );
     } else {
-
-        // if true, open .browserslistrc for writing
-        let outputFile = fs.createWriteStream(process.cwd() + '/.browserslistrc');
-        outputFile.on('error', function (err) { /* error handling */ });
-        // write a header comment so it's clear where the config comes from
-        outputFile.write("# This file was created by bl2bl2. \n# Learn more at https://github.com/tonypconway/bl2bl\n");
-        // then write each min browser version to the output file
-        browserslistOutput.forEach(
-            function (v) {
-                outputFile.write(v + '\n');
-            }
+      // Check if packageJSON.browserslist exists
+      if (packageJSON.browserslist) {
+        // Write each item of the array on a new line in .browserslistrc_backup
+        const backupData = packageJSON.browserslist.join("\n");
+        fs.writeFileSync(
+          process.cwd() + "/.browserslistrc_backup",
+          backupMessage + backupData,
+          { encoding: "utf8" },
         );
-        // and close the file
-        outputFile.end();
-
-        // then remove browserslist from packageJSON if it exists to avoid conflicts/errors
-        if (packageJSON.browserslist) {
-            delete packageJSON.browserslist;
-        }
+      } else {
+        console.log("No existing browserslist configuration, skipping backup.");
+      }
     }
+  }
 
-    // Whatever happens, update package.json as long as the packageJSON object isn't null
-    if (packageJSON != null) {
-        fs.writeFileSync(process.cwd() + '/package.json', JSON.stringify(packageJSON, null, 2), { encoding: 'utf8' });
+  let baselineVersions;
+  let browserslistOutput = new Array();
+
+  const bl2blConfig = packageJSON.bl2bl;
+
+  // If the user wants Baseline Widely Available
+  if (bl2blConfig.baselineThreshold === "widely available") {
+    // Get the minimum Baseline widely available versions
+    baselineVersions = bbm.getMinimumWidelyAvailable(
+      packageJSON.bl2bl.downstreamBrowsers,
+    );
+  } else if (
+    // If they've passed a valid year, i.e. after BL starts and not the current or future years
+    parseInt(bl2blConfig.baselineThreshold) >= 2016 &&
+    parseInt(bl2blConfig.baselineThreshold) <= new Date().getFullYear() - 1
+  ) {
+    // Get the versions that correspond to those years
+    baselineVersions = bbm.getMinimumByYear(
+      bl2blConfig.baselineThreshold,
+      bl2blConfig.downstreamBrowsers,
+    );
+  } else {
+    // If the baselineThreshold doesn't meet any of those criteria, there's a problem.  Time to bounce.
+    console.warn(
+      "there's a problem with your bl2bl config.\nPlease fix it before proceeding.",
+    );
+    process.exit();
+  }
+
+  // Loop through returned minimum browser versions
+  baselineVersions.forEach((browser) => {
+    // Check if that browser is supported by Browserslist
+    if (Object.keys(browsers).includes(browser.browser)) {
+      // And add it to the list of versions to be written
+      browserslistOutput.push(
+        `${browsers[browser.browser]} >= ${browser.version}`,
+      );
     }
+  });
 
+  // Behaviour varies depending on whether userBrowserslistrc=true or false
+  if (!bl2blConfig.useBrowserslistrc) {
+    // if false, add baselineVersions to packageJSON object for later
+    packageJSON["browserslist"] = browserslistOutput;
+    // and delete any existing .browserslistrc files to avoid conflict
+    fs.access(process.cwd() + "/.browserslistrc", (err) => {
+      if (!err) {
+        fs.unlink(process.cwd() + "/.browserslistrc", (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+  } else {
+    // if true, open .browserslistrc for writing
+    let outputFile = fs.createWriteStream(process.cwd() + "/.browserslistrc");
+    outputFile.on("error", function (err) {
+      /* error handling */
+    });
+    // write a header comment so it's clear where the config comes from
+    outputFile.write(
+      "# This file was created by bl2bl2. \n# Learn more at https://github.com/tonypconway/bl2bl\n",
+    );
+    // then write each min browser version to the output file
+    browserslistOutput.forEach(function (v) {
+      outputFile.write(v + "\n");
+    });
+    // and close the file
+    outputFile.end();
+
+    // then remove browserslist from packageJSON if it exists to avoid conflicts/errors
+    if (packageJSON.browserslist) {
+      delete packageJSON.browserslist;
+    }
+  }
+
+  // Whatever happens, update package.json as long as the packageJSON object isn't null
+  if (packageJSON != null) {
+    fs.writeFileSync(
+      process.cwd() + "/package.json",
+      JSON.stringify(packageJSON, null, 2),
+      { encoding: "utf8" },
+    );
+  }
 });
