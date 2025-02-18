03b8a5ea1ba8fa57c0c804fa6b8e756749a99800
Rick Viscomi <rviscomi@users.noreply.github.com>
2025-02-17 19:21:22 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -21,20 +21,12 @@ import("baseline-browser-mapping").then((bbm) => {
 
   const fs = require("node:fs");
 
-  let incomingConfig = packageJSON.bl2bl ? packageJSON.bl2bl : {};
+  let incomingConfig = packageJSON.bl2bl ?? {};
   const bl2blConfig = {
-    baselineThreshold: incomingConfig.baselineThreshold
-      ? incomingConfig.baselineThreshold
-      : "widely available",
-    useBrowserslistrc: incomingConfig.useBrowserslistrc
-      ? incomingConfig.useBrowserslistrc
-      : false,
-    downstreamBrowsers: incomingConfig.downstreamBrowsers
-      ? incomingConfig.downstreamBrowsers
-      : false,
-    savePrevious: incomingConfig.savePrevious
-      ? incomingConfig.savePrevious
-      : true,
+    baselineThreshold: incomingConfig.baselineThreshold ?? "widely available",
+    useBrowserslistrc: incomingConfig.useBrowserslistrc ?? false,
+    downstreamBrowsers: incomingConfig.downstreamBrowsers ?? false,
+    savePrevious: incomingConfig.savePrevious ?? true,
   };
 
   packageJSON.bl2bl = bl2blConfig;
